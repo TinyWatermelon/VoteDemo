323047eb4ce81522254019f6d8dadd39b08b8ba2
louis <louisleechn@gmail.com>
2019-04-02 21:02:19 +0800

    Delete test.js

--- webapp/test.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-const ccpPath = path.resolve(__dirname, '..', 'network', 'connection.json');
-const ccpJSON = fs.readFileSync(ccpPath, 'utf8');
-const ccp = JSON.parse(ccpJSON);
-console.log(`ccp path: ${ccp.toStiring()}`);
