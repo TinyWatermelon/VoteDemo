f861a6157f1a37a6e2c18476a5bef739c8a7e940
louis <louisleechn@gmail.com>
2019-04-23 20:42:23 +0800

    Update index.js

--- webapp/routers/index.js
+++ webapp/routers/index.js
@@ -2,7 +2,8 @@ const router = require('koa-router')()
 const IndexController = require('./../controllers/index')
 
 router.get('/',IndexController.indexPage)
-    .get('/voteUserUser', IndexController.voteUser)
-	.get('/getUserVote',IndexController.getUserVote)
+        .get('/voteUser', IndexController.voteUser)
+        .get('/getUserVote',IndexController.getUserVote)
+        .get('/getUserVoteById',IndexController.getUserVoteById)
 
 module.exports = router
