696382b808227808c0ed729db0d8664fee2d40cb
louis <louisleechn@gmail.com>
2019-04-24 18:33:43 +0800

    Add files via upload

--- webapp/routers/index.js
+++ webapp/routers/index.js
@@ -1,9 +1,13 @@
 const router = require('koa-router')()
 const IndexController = require('./../controllers/index')
+const VoteController = require('./../controllers/vote')
+const QueryController = require('./../controllers/query')
 
 router.get('/',IndexController.indexPage)
-        .get('/voteUser', IndexController.voteUser)
-        .get('/getUserVote',IndexController.getUserVote)
-        .get('/getUserVoteById',IndexController.getUserVoteById)
+	.get('/votePage',VoteController.initPage)
+	.get('/queryPage',QueryController.initPage)
+	.post('/voteUser', IndexController.voteUser)
+	.get('/getUserVote',IndexController.getUserVote)
+	.get('/getUserVoteById',IndexController.getUserVoteById)
 
 module.exports = router
