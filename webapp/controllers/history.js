623cc00f7342a664d5b2f6e5dda89623ae5a0edb
louis <louisleechn@gmail.com>
2019-06-02 13:19:53 +0800

    Add files via upload

---
+++ webapp/controllers/history.js
@@ -0,0 +1,49 @@
+const { FileSystemWallet, Gateway } = require('fabric-network');
+const fs = require('fs');
+const path = require('path');
+
+const ccpPath = path.resolve(__dirname, '..','..', 'network', 'connection.json');
+const ccpJSON = fs.readFileSync(ccpPath, 'utf8');
+const ccp = JSON.parse(ccpJSON);
+
+
+module.exports = {
+    async initPage ( ctx ) {
+        const title = 'history page'
+
+        let result = ''
+		try {
+			//const peer = new Peer("grpc://localhost:7051");
+			
+			const walletPath = path.join(process.cwd(), 'wallet');
+			const wallet = new FileSystemWallet(walletPath);
+			console.log(`Wallet path: ${walletPath}`);
+
+			const userExists = await wallet.exists('louis');
+			if (!userExists) {
+				console.log('An identity for the user "louis" does not exist in the wallet');
+				console.log('Run the registerUser.js application before retrying');
+				return;
+			}
+
+			const gateway = new Gateway();
+			await gateway.connect(ccp, { wallet, identity: 'louis', discovery: { enabled: false } });
+			console.log('creating a new gateway');
+			const network = await gateway.getNetwork('mychannel');
+
+			console.log('getting the channel');
+			const channel = network.getChannel();
+			result = await channel.queryInfo();
+			result = JSON.stringify(result);
+			console.log(`Transaction has been evaluated, result is: ${result}`);
+			
+	    } catch (error) {
+		console.error(`Failed to evaluate transaction: ${error}`);
+	    }
+
+        await ctx.render('history', {
+            title, result
+        })
+    }
+
+}
